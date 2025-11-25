4a3dba1997d25acf51fb1a613edb1451e625ac50
YoonGangHoon <yghun021007@naver.com>
2025-11-25 16:09:12 +0900

    refactor: LOMBOK 사용

--- src/main/java/knu/atoz/document/Document.java
+++ src/main/java/knu/atoz/document/Document.java
@@ -1,39 +1,21 @@
 package knu.atoz.document;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Document {
-    Long id;          // PK (document_id)
-    Long projectId;   // FK (project_id)
+    Long id;
+    Long projectId;
     String title;
     String location;
-
 
     public Document(Long projectId, String title, String location) {
         this.projectId = projectId;
         this.title = title;
         this.location = location;
-    }
-
-    public Document(Long id, Long projectId, String title, String location) {
-        this.id = id;
-        this.projectId = projectId;
-        this.title = title;
-        this.location = location;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getProjectId() {
-        return projectId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getLocation() {
-        return location;
     }
 }
